a5854d103c5ad13568b15637e9f852ae9f62c3a8
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2024-08-21 15:30:48 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,3 @@
+const message = 'hihdhw';
+
+console.log(message);
---
+++ src/scripts/generateContacts.js
@@ -0,0 +1,6 @@
+import { PATH_DB } from '../constants/contacts.js';
+
+const generateContacts = async (number) => {};
+
+await generateContacts(5);
+
